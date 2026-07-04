43487bf6eee1c085a1d6bb4e284a1e6d1fcd16d2
Deyvid Santos <deyvidsantosdatascience@gmail.com>
2026-07-04 10:51:11 -0300

    feat: lojitas entities and enums for modeling data

---
+++ app/src/main/java/com/capgemini/deyvidsilva/lojitas/domain/entity/CarrinhoItem.kt
@@ -0,0 +1,6 @@
+package com.capgemini.deyvidsilva.lojitas.domain.entity
+
+data class CarrinhoItem(
+    val produto: Produto,
+    val quantidade: Int
+)
---
+++ app/src/main/java/com/capgemini/deyvidsilva/lojitas/domain/entity/Categoria.kt
@@ -0,0 +1,6 @@
+package com.capgemini.deyvidsilva.lojitas.domain.entity
+
+data class Categoria(
+    val id: String,
+    val nome: String
+)
